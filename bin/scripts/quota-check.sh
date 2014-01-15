6fea761fae595e4179a06c841c326b6b356dcf04
Michal Fojtik <mfojtik@redhat.com>
2014-01-15 14:38:36 +0100

    Initial import

---
+++ bin/scripts/quota-check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This code was originally created by Marek Mahut <mmahut@redhat.com>
+# https://github.com/mmahut/openshift-quota-monitor-cartridge/blob/master/lib/functions
+
+# returns the used space from quota command
+function get_used_space() {
+  echo $(quota -w|egrep -v '(quotas|Filesystem)'|awk -F ' ' '{ print $2}')
+}
+
+# returns the total quota for your gear
+function get_quota() {
+  echo $(quota -w|egrep -v '(quotas|Filesystem)'|awk -F ' ' '{ print $4}')
+}
+
+used_space=$(get_used_space)
+quota=$(get_quota)
+percentage=$(echo "(${used_space}/${quota})*100" |bc -l|sed 's/^\./0./')
+echo ${percentage/\.*}
