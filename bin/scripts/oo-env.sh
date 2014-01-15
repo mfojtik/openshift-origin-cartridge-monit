8d890686d1deaeef8b3095512e9e74b7a3b54bc1
Michal Fojtik <mfojtik@redhat.com>
2014-01-15 18:32:44 +0100

    Fixed broken OpenShift ENV variables

---
+++ bin/scripts/oo-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Wrapper script for commands executed from monit start/stop programm.
+# Due to limitation of monit[1] we need to restore all environment variables
+# in order to make the cartridge control scripts to succeed.
+#
+# [1] http://mmonit.com/wiki/Monit/FAQ#execution
+#
+
+if [ ! -f ~/monit/var/env ]; then
+  echo "ERROR: Unable to restore OpenShift environment."
+  exit 1
+fi
+
+source ~/monit/var/env
+
+exec "$@" >>${OPENSHIFT_MONIT_DIR}log/monit.log 2>&1
